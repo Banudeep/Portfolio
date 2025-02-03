1eb5e09f12b030cbbc6111ac2073d4b97e722665
Banudeep <60642712+Banudeep@users.noreply.github.com>
2025-02-03 16:46:37 -0500

    icon changed

---
+++ src/Components/Hero/Hero.jsx
@@ -0,0 +1,14 @@
+// import styles from './Hero.module.css'
+
+function Hero() {
+  return (
+    <><img/>
+        <div className="">
+        <h1>Banudeep Reddy Gade</h1>
+        </div>
+
+    </>
+  )
+}
+
+export default Hero
