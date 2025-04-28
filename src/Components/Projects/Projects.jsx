42eeffc315b46f8fb8c132172735d820d4f9d609
Banudeep <60642712+Banudeep@users.noreply.github.com>
2025-04-28 13:27:55 -0400

    logo zentrail

--- src/Components/Projects/Projects.jsx
+++ src/Components/Projects/Projects.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import styles from "./ProjectsStyles.module.css";
 import github_icon from "../../assets/github_icon.png";
+import zentrail from "../../assets/zentrail.png";
 
 function Projects() {
   return (
@@ -8,9 +9,14 @@ function Projects() {
       {/* <br></br> */}
       <h1>projects</h1>
       <div className={styles.projectsContainer}>
-        <a href="" target="_blank">
-          <img className="hover" src={github_icon} alt="project Picture" />
-          <h3>Project Name</h3>
+        <a href="https://github.com/Banudeep/zentrail_bitcamp" target="_blank">
+          <img
+            className="hover"
+            src={zentrail}
+            alt="project Picture"
+            style={{ width: "300px", height: "300px", objectFit: "fill" }}
+          />
+          <h3>Zentrail</h3>
           <p> description </p>
         </a>
         <a href="" target="_blank">
